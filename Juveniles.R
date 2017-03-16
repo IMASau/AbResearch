5b8fc2c8c264c06b17a4a74237e6bf5b2e289f49
Craig Mundy <Craig.Mundy@utas.edu.au>
2017-03-17 10:27:08 +1100

    Improvements to generating counts/plate

--- Juveniles.R
+++ Juveniles.R
@@ -18,10 +18,37 @@ juv <- rename(juv, survdate = date)
 juv$string <- as.factor(juv$string)
 juv$plate <- as.factor(juv$plate)
 
+platearea <- 0.503
 
 juv.sl <- filter(juv, !is.na(ab_sl))
 
 #juv$ab_sl <- NAToUnknown(x = juv$ab_sl, unknown = 0)
+
+#ab_count <- ddply(juv, "survdate", summarise, ab_ct = length(ab_sl))
+
+## Because sites were surveyed on different days, can't do a fully crossed complete() function
+#ab_count <- filter(juv, !is.na(ab_sl) & ab_sl <= 80) %>%
+
+juv$survindex <- as.factor(paste(juv$location, juv$survdate, juv$string, juv$plate, sep="_"))
+
+ab_count <- filter(juv, ab_sl >=40 & ab_sl <= 60) %>% 
+ group_by(survindex) %>%
+ summarise(ab_n =n()) %>%  #as.data.frame()
+ complete(survindex, fill = list(ab_n = 0)) %>%
+ as.data.frame()
+ 
+ab_count$absm <- ab_count$ab_n * (1/platearea)
+ab2plus <- extract(ab_count, survindex, regex = "_", into = c("location", "survdate", "string","plate"))
+
+
+ab2plus <- data.frame(colsplit(ab_count$survindex, pattern="_", c("location", "survdate", "string","plate")), ab_count$survindex, ab_count$ab_n, ab_count$absm)
+
+df <- data.frame(x = c(NA, "a-b", "a-d", "b-c", "d-e"))
+df %>% extract(x, "A")
+df %>% extract(x, c("A", "B"), "([[:alnum:]]+)-([[:alnum:]]+)")
+
+
+ab2plus <- 
 
 # #replace NA with 0
 # juv[is.na(juv)] <- 0
@@ -31,7 +58,6 @@ juv<-juv[1:6]
 #remove NA's
 juv <- juv[complete.cases(juv[,5]),]
 summary(juv)
-
 
 # SORTING OUT THE DATES SO THEY ARE IN USEFUL FORMAT
 #juv$survdate<-as.survdate(juv$survdate, origin, format="%d/%m/%y")
@@ -51,7 +77,7 @@ summary(juv)
 
 ggplot(juv.sl, aes(x=ab_sl, color=location))+
   geom_histogram(stat = "bin", colour="grey", binwidth = 5)+
-  scale_x_continuous(limits=c(0, 150))+
+  scale_x_continuous(limits=c(0, 150)) +
   
   xlab("Shell Length (mm)") + ylab("Frequency") +
   theme_bw()
@@ -67,14 +93,6 @@ ggplot(juv.sl, aes(x=ab_sl)) +
   theme_bw()+
   facet_grid(. ~ location)
 
-ab_count <- ddply(juv, "survdate", summarise, ab_ct = length(ab_sl))
-
-## Because sites were surveyed on different days, can't do a fully crossed complete() function
-ab_count <- filter(juv, !is.na(ab_sl)) %>% group_by(location, survdate, string) %>%
-summarise(ab_ct =n()) %>% as.data.frame()
- 
-complete(location, string, plate, fill = list(ab_ct = 0)) %>%
- 
 
 
 #rearregage DF giving number of juveniles by location, survdate and string.
