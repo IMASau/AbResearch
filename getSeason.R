fc6d544f1eef888ee0ebda3bfa3f319b5bb290d8
Craig Mundy <Craig.Mundy@utas.edu.au>
2017-04-09 22:02:24 +1000

    Edits - including the stderr functions

--- getSeason.R
+++ getSeason.R
@@ -17,3 +17,17 @@ getSeason <- function(DATES) {
 
 
 #datain$season <- getSeason(datain$pred.date)
+
+
+stderr <- function(x) {
+ sqrt(var(x[!is.na(x)]) / length(x[!is.na(x)]))
+}
+
+my.stderr <- function(x) {
+ meany <- mean(x)
+ ymin  <- mean(x) - stderr(x)
+ ymax  <- mean(x) + stderr(x)
+ # assemble the named output
+ out <- c(y = meany, ymin = ymin, ymax = ymax)
+ return(out)
+}
