0a7a5637b1f09966488b5e0a84720a86cc8f6565
Craig Mundy <Craig.Mundy@utas.edu.au>
2018-07-19 12:26:49 +1000

    Edits & challenge note for Jaime

--- MM/MM_Misc.R
+++ MM/MM_Misc.R
@@ -1,18 +1,37 @@
+compiled.df.backup <-  compiled.df
+
+
 df <- data.frame(x = c("x: 123", "y: error: 7"))
 df %>% separate(x, c("key", "value", "third"), ": ", extra = "merge")
 df %>% separate(x, into=c("key", "value", "third"), sep= ":", fill= "right" )
 
 #colnames(compiled.df) <- tolower(colnames(compiled.df))
 
-test <- compiled.df %>% separate(subblocklist, into=c("sub1", "sub2", "sub3", "sub4"), sep = ", " , fill= "right", remove = FALSE ) %>%
+compiled.df  <- compiled.df %>% separate(blocklist, into=c("bl1", "bl2", "bl3", "bl4", "bl5"), sep = ", " , fill= "right", remove = FALSE ) %>%
  as.data.frame()
 
 ## recode fishyear
 compiled.df$fishyear2 <- year(compiled.df$msr.date)
 
- block13E <- filter(compiled.df, grepl("13E", subblocklist))
 
-ggplot(block13E) + geom_histogram(aes(x=shell.length, group = fishyear2)) + facet_grid(~ fishyear2)
+## see the link below
+##https://dplyr.tidyverse.org/reference/filter_all.html
+##work out a way to filter for multiple blocks on multiple columns
+##Note: the grepl function pulls out out nore records than we want e.g.
+## grepl ("9" ...  finds all rows with a 9 in the blocklist column (e.g. 9, 19, 29 etc)
+## So this is not what we want
+
+
+
+## Filter data by block
+x <- c("10", "9", "11")
+plotdat <- filter(compiled.df, grepl("9", blocklist), between(shell.length, 132, 200) )
+plotdat <- filter(compiled.df, grepl(paste(x, collapse = "|"), blocklist), between(shell.length, 132, 200) )
+unique(plotdat$blocklist)
+
+ggplot(plotdat) + geom_histogram(aes(x=shell.length, group = fishyear2)) + facet_grid(~ fishyear2)
+ggplot(plotdat) + geom_density(aes(x=shell.length, group = factor(fishyear2), colour = factor(fishyear2), alpha= 2)) + scale_fill_brewer(palette = "Dark2")
+
 
 table(compiled.df$fishyear, compiled.df$fishyear2)
 table(block13E$fishyear, block13E$fishyear2)
@@ -23,4 +42,5 @@ length(pick)
 
 head(compiled.df[pick,])
 
+compiled.df[423860,]
 
