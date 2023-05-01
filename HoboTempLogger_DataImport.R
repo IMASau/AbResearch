e5058029fb2957c277d46659dc154814873b8ca5
jaimemcallister <jaime.mcallister@utas.edu.au>
2023-05-01 14:15:54 +1000

    mean temperature for sites

--- HoboTempLogger_DataImport.R
+++ HoboTempLogger_DataImport.R
@@ -20,7 +20,7 @@ logger.b <- read.csv("C:/Users/jaimem/OneDrive - University of Tasmania/Document
 logger.b <- logger.b %>%
  dplyr::rename(date.time = 2, temperature = 3) %>%  
  mutate(date.time = as.POSIXct(date.time, format="%y-%m-%d %H:%M:%S", tz = 'Australia/Tasmania'),
-        treatment = 'mid') %>% 
+        treatment = 'medium') %>% 
  select(date.time, temperature, treatment)
         
 logger.c <- read.csv("C:/Users/jaimem/OneDrive - University of Tasmania/Documents/AB_proteomics/1_abalone_physiology_TreatmentB_19degrees.csv",
@@ -33,11 +33,11 @@ logger.c <- logger.c %>%
  select(date.time, temperature, treatment)
 
 logger.dat <- bind_rows(logger.a, logger.b, logger.c) %>% 
- mutate(treatment = factor(treatment, levels = c('control', 'mid', 'high')))
+ mutate(treatment = factor(treatment, levels = c('high', 'medium', 'control')))
 
-logger.dat %>% 
+logger_plot <- logger.dat %>% 
  filter(date.time > ymd_hms("2022-12-12 12:40:00") & date.time < ymd_hms("2022-12-19 05:00:00") & treatment == 'high'|
-         date.time > ymd_hms("2022-12-12 12:40:00") & date.time < ymd_hms("2022-12-19 14:00:00") & treatment %in% c('control', 'mid')) %>%  
+         date.time > ymd_hms("2022-12-12 12:40:00") & date.time < ymd_hms("2022-12-19 14:00:00") & treatment %in% c('control', 'medium')) %>%  
  ggplot(aes(x = date.time, y = temperature))+
  geom_line(aes(color = treatment))+
  geom_vline(aes(xintercept = ymd_hms("2022-12-14 00:00:00")), colour = 'red', linetype = 'dashed')+
@@ -45,8 +45,23 @@ logger.dat %>%
  theme_bw()+
  xlab('Date.Time')+
  ylab('Temperature (\u00B0C)')+
- scale_color_discrete(name = "Treatment", labels = c("Control", "Mid", "High"))+
- scale_y_continuous(breaks = seq(12,20,1))
+ scale_color_discrete(name = "Treatment", labels = c("High", "Medium", "Control"))+
+ scale_y_continuous(breaks = seq(12,20,1))+
+ # geom_hline(aes(yintercept = 13.8), linetype = 'dashed', colour = '#F8766D')+
+ # geom_hline(aes(yintercept = 15.4), linetype = 'dashed', colour = '#00BA38')+
+ # geom_hline(aes(yintercept = 18.2), linetype = 'dashed', colour = '#619CFF')
+ geom_segment(aes(x = ymd_hms("2022-12-16 00:00:00"), xend = ymd_hms("2022-12-19 05:00:00"), y = 13.8, yend = 13.8), linetype = 'dashed', colour = '#619CFF')+
+ geom_segment(aes(x = ymd_hms("2022-12-16 00:00:00"), xend = ymd_hms("2022-12-19 05:00:00"), y = 15.4, yend = 15.4), linetype = 'dashed', colour = '#00BA38')+
+ geom_segment(aes(x = ymd_hms("2022-12-16 00:00:00"), xend = ymd_hms("2022-12-19 05:00:00"), y = 18.2, yend = 18.2), linetype = 'dashed', colour = '#F8766D')+
+ geom_text(label = '13.8 \u00B0C', x = ymd_hms("2022-12-19 15:00:00"), y = 13.8, colour = '#619CFF', size = 3, stat = 'identity')+
+ geom_text(label = '15.4 \u00B0C', x = ymd_hms("2022-12-19 15:00:00"), y = 15.4, colour = '#00BA38', size = 3, stat = 'identity')+
+ geom_text(label = '18.2 \u00B0C', x = ymd_hms("2022-12-19 15:00:00"), y = 18.2, colour = '#F8766D', size = 3, stat = 'identity')+
+ geom_text(label = 'Aclimatisation', x = ymd_hms("2022-12-13 00:00:00"), y = 12.5, size = 3, stat = 'identity')+
+ geom_text(label = 'Manipulation', x = ymd_hms("2022-12-15 00:00:00"), y = 12.5, size = 3, stat = 'identity')+
+ geom_text(label = 'Heat Stress', x = ymd_hms("2022-12-18 00:00:00"), y = 12.5, size = 3, stat = 'identity')+
+ theme(legend.position = c(0, 1),
+       legend.justification = c(0, 1),
+       legend.background = element_rect(fill = "white", color = "black", linewidth = 0.2))
 
 
 logger.dat %>% 
@@ -55,3 +70,9 @@ logger.dat %>%
  summarise(average.temp = mean(temperature),
            max.temp = max(temperature),
            min.temp = min(temperature))
+
+ggsave(filename = paste('C:/Users/jaimem/OneDrive - University of Tasmania/Documents/AB_proteomics/HeatStressExperiment_LoggerTemperature', '.pdf', sep = ''), 
+       plot = logger_plot, units = 'mm', width = 190, height = 200)
+
+ggsave(filename = paste('C:/Users/jaimem/OneDrive - University of Tasmania/Documents/AB_proteomics/HeatStressExperiment_LoggerTemperature', '.png', sep = ''), 
+       plot = logger_plot, units = 'mm', width = 190, height = 200)
--- NOAA_SST_Plots_JaimeMcAllister.R
+++ NOAA_SST_Plots_JaimeMcAllister.R
@@ -50,6 +50,17 @@ ggsave(filename = paste('C:/cloudstor/R_Stuff/WEI/Results/EastcoastMeanAnnual_SS
        plot = sst_plot, units = 'mm', width = 190, height = 200)
 
 ##---------------------------------------------------------------------------##
+# Physiology marine heat wave event data
+
+df.1 <- physio_dat %>%
+ mutate(samp_season = getSeason(date),
+        samp_season = if_else(site %in% c('SIS', 'THU') & date %in% c(as.Date('2022-06-16')), 'Autumn',
+                              if_else(site %in% c('GAR') & date %in% c(as.Date('2022-06-21')), 'Autumn', samp_season))) %>% 
+ filter(site == 'ACT' & 
+         samp_season == 'Summer'|
+         grepl('Additional', comments))
+
+##---------------------------------------------------------------------------##
 # SST plot for physiology sites in 2021-2022
 # Plot of daily water temperature of each site in sampling period 2021-2022 and sampling periods
 
@@ -61,6 +72,26 @@ noaa_physio_site_dat <- noaa_aus %>%
  filter(sau_name %in% c('AB13', 'AB14', 'AB22', 'AB23', 'AB28', 'AB30'),
         between(samp_year, 2021, 2022))
 
+# Add site names to match sau_name
+noaa_physio_site_dat <- noaa_physio_site_dat %>% 
+ mutate(site = case_when(sau_name == 'AB13' ~ 'MOL',
+                         sau_name == 'AB14' ~ 'ACT',
+                         sau_name == 'AB22' ~ 'THU',
+                         sau_name == 'AB23' ~ 'SIS',
+                         sau_name == 'AB28' ~ 'SEY',
+                         sau_name == 'AB30' ~ 'GAR'))
+ 
+noaa_physio_site_dat$site <- factor(noaa_physio_site_dat$site, levels = c('GAR', 'SEY', 'SIS', 'THU', 'ACT', 'MOL'))
+
+df_1 <- noaa_site_dat %>% 
+ group_by(site) %>% 
+ summarise(min_temp = min(C),
+           max_temp = max(C),
+           mean_temp = mean(C),
+           sd_temp = sd(C),
+           n_temp = n(),
+           se_temp = sd_temp/sqrt(n_temp))
+
 
 # Add seasonal variable noting adjustment for late Autumn sampling at some sites
 physio_season_dat <- physio_dat %>% 
@@ -70,6 +101,7 @@ physio_season_dat <- physio_dat %>%
 
 # Summarise min and max sampling dates for each season
 physio_season_dates <- physio_season_dat %>% 
+ filter(!grepl('Additional', comments)) %>% 
  group_by(samp_season) %>% 
  summarise(min_date = min(date),
            max_date = max(date)) %>% 
@@ -77,22 +109,27 @@ physio_season_dates <- physio_season_dat %>%
         cols = c('green', 'purple', 'red', 'blue'))
 
 # Create vector of plot colours for sites to match metabolomics analysis
-cols <- c('green', 'blue', 'yellow', 'orange', 'purple', 'red')
+plot_cols <- c('red', 'purple', 'orange', 'yellow3', 'green3', 'blue')
+plot_labs <- c('GAR', 'SEY', 'SIS', 'THU', 'ACT', 'MOL')
 
 physio_site_plot <- noaa_physio_site_dat %>% 
- ggplot(aes(x = as.Date(timestamp), y = C, group = sau_name, colour = sau_name))+
+ ggplot(aes(x = as.Date(timestamp), y = C, group = site, colour = site))+
  geom_line()+
  theme_bw()+
  xlab('Year')+
  ylab(expression("Temperature " ( degree~C)))+
  geom_vline(data = physio_season_dates, aes(xintercept = as.Date(min_date)), linetype = 'dashed', alpha = 0.2)+
  geom_vline(data = physio_season_dates, aes(xintercept = as.Date(max_date)), linetype = 'dashed', alpha = 0.2)+
- scale_colour_manual(name = '', values = cols, labels = c('ACT', 'MOL', 'THU', 'SIS', 'SEY', 'GAR'))+
+ scale_colour_manual(name = '', values = plot_cols, labels = plot_labs)+
  scale_x_date(date_labels = '%b-%Y')+
  geom_rect(data= physio_season_dates, inherit.aes = FALSE,
            aes(xmin = min_date, xmax = max_date, ymin = -Inf, ymax = Inf, group = samp_season, fill = samp_season), 
            alpha = 0.2)+
- scale_fill_manual(values = c('blue', 'purple', 'red', 'green'), name = '')
+ scale_fill_manual(values = c('blue', 'purple', 'red', 'green3'), name = '')
+ # theme(legend.title = element_blank(),
+ #       legend.position = c(0, 1),
+ #       legend.justification = c(0, 1),
+ #       legend.background = element_rect(fill = "white", color = "black", linewidth = 0.2))
 
 ggsave(filename = paste('C:/cloudstor/R_Stuff/WEI/Results/PhysiologySeasonalSiteTemperature_2021-2022', '.pdf', sep = ''), 
        plot = physio_site_plot, units = 'mm', width = 190, height = 200)
@@ -101,14 +138,63 @@ ggsave(filename = paste('C:/cloudstor/R_Stuff/WEI/Results/PhysiologySeasonalSite
        plot = physio_site_plot, units = 'mm', width = 190, height = 200)
 
 ##---------------------------------------------------------------------------##
+# SST plot for physiology sites historical
+# Plot of daily water temperature of each site using all historical data
+
+# Select NOAA data for physiology site Blocks/SAUs
+noaa_site_dat <- noaa_aus %>% 
+ mutate(samp_year = year(timestamp),
+        samp_month = month(timestamp),
+        month_year = paste(samp_year, samp_month, sep = '_')) %>% 
+ filter(sau_name %in% c('AB13', 'AB14', 'AB22', 'AB23', 'AB28', 'AB30'))
+
+# Add site names to match sau_name
+noaa_site_dat <- noaa_site_dat %>% 
+ mutate(site = case_when(sau_name == 'AB13' ~ 'MOL',
+                         sau_name == 'AB14' ~ 'ACT',
+                         sau_name == 'AB22' ~ 'THU',
+                         sau_name == 'AB23' ~ 'SIS',
+                         sau_name == 'AB28' ~ 'SEY',
+                         sau_name == 'AB30' ~ 'GAR'))
+
+noaa_site_dat$site <- factor(noaa_site_dat$site, levels = c('GAR', 'SEY', 'SIS', 'THU', 'ACT', 'MOL'))
+
+# Create vector of plot colours for sites to match metabolomics analysis
+plot_cols <- c('red', 'purple', 'orange', 'yellow3', 'green3', 'blue')
+plot_labs <- c('GAR', 'SEY', 'SIS', 'THU', 'ACT', 'MOL')
+
+physio_site_temperature_plot <- noaa_site_dat %>% 
+ ggplot(aes(x = as.Date(timestamp), y = C, group = site, colour = site))+
+ # geom_line()+
+ geom_smooth(method = lm, se = F)+
+ scale_y_continuous(breaks = seq(13, 16, 0.5))+
+ theme_bw()+
+ xlab('Year')+
+ ylab(expression("Temperature " ( degree~C)))+
+ scale_colour_manual(name = '', values = plot_cols, labels = plot_labs)+
+ scale_x_date(date_labels = '%b-%Y')+
+ theme(legend.title = element_blank(),
+       legend.position = c(0, 1),
+       legend.justification = c(0, 1),
+       legend.background = element_rect(fill = "white", color = "black", linewidth = 0.2))
+
+ggsave(filename = paste('C:/cloudstor/R_Stuff/WEI/Results/PhysiologySeasonalSiteTemperature_1981-2022', '.pdf', sep = ''), 
+       plot = physio_site_temperature_plot, units = 'mm', width = 190, height = 200)
+
+ggsave(filename = paste('C:/cloudstor/R_Stuff/WEI/Results/PhysiologySeasonalSiteTemperature_1981-2022', '.png', sep = ''), 
+       plot = physio_site_temperature_plot, units = 'mm', width = 190, height = 200)
+
+##---------------------------------------------------------------------------##
 # WEI for physiology sites
 
+# Load wave exposure index for points every 100 m along Tasmanian coastline
 wei_2010 <- readRDS('C:/Users/jaimem/Dropbox/AbaloneData/WEI_2010.rds')
 
 wei_2010 <- wei_2010 %>% 
  st_as_sf() %>% 
  st_transform(st_crs(7855))
 
+# Load physiology sampling sites
 physio_sites <- read.xlsx("C:/Users/jaimem/OneDrive - University of Tasmania/Documents/AB_proteomics/Abalone_ProteomicCollection_Sites_July2021.xlsx",
                        detectDates = T)
 
@@ -118,21 +204,41 @@ physio_sites_sf <- physio_sites %>%
  st_as_sf(coords = c('longitude', 'latitude'), crs = 4326) %>% 
  st_transform(st_crs(7855))
   
+# Join physiology sites to nearest WEI point along coastline
 physio_df <- st_join(physio_sites_sf, wei_2010, join = st_nearest_feature)
 
-physio_df$label <- factor(physio_df$label, levels = c('MOL', 'ACT', 'THU', 'SIS', 'SEY', 'GAR'))
+plot_cols <- c('red', 'purple', 'orange', 'yellow3', 'green3', 'blue')
+plot_labs <- c('GAR', 'SEY', 'SIS', 'THU', 'ACT', 'MOL')
 
+physio_df$label <- factor(physio_df$label, levels = c('GAR', 'SEY', 'SIS', 'THU', 'ACT', 'MOL'))
+
+# Quick plot of annual WEI for each site
 physio_df %>% ggplot(aes(x = label, y = waveyear))+
  geom_bar(stat = 'identity')+
  theme_bw()
 
-df.2 <- physio_df %>%
+# Re-format monthly WEI variable names and values for plotting
+physio_wei_df <- physio_df %>%
  gather(key = 'wei_month', value = 'wei', month_1:month_12) %>%
  mutate(wei_month = month.abb[as.numeric(gsub('month_', '', wei_month))],
         wei_month = factor(wei_month, levels = month.abb))
  
 
-df.2 %>% ggplot(aes(x = wei_month, y = wei, group = label, colour = label))+
- geom_line()
+physio_wei_plot <- physio_wei_df %>% 
+ ggplot(aes(x = wei_month, y = wei, group = label, colour = label))+
+ geom_line()+
+ geom_point()+
+ theme_bw()+
+ ylab('WEI')+
+ xlab('Month')+
+ scale_colour_manual(name = '', values = plot_cols, labels = plot_labs)+
+ theme(legend.title = element_blank(),
+  legend.position = c(0, 1),
+       legend.justification = c(0, 1),
+       legend.background = element_rect(fill = "white", color = "black", linewidth = 0.2))
 
+ggsave(filename = paste('C:/cloudstor/R_Stuff/WEI/Results/PhysiologySeasonalSiteWEI', '.pdf', sep = ''), 
+       plot = physio_wei_plot, units = 'mm', width = 190, height = 200)
 
+ggsave(filename = paste('C:/cloudstor/R_Stuff/WEI/Results/PhysiologySeasonalSiteWEI', '.png', sep = ''), 
+       plot = physio_wei_plot, units = 'mm', width = 190, height = 200)
